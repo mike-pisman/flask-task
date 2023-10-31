4657bf9fe6dc4067c8407ac441eead68f0d13af6
Michael Pisman <mike.pisman@gmail.com>
2023-10-31 13:52:26 -0600

    feat: Added abstract Model

--- app/models.py
+++ app/models.py
@@ -1,20 +1,53 @@
 from datetime import datetime
 from . import db
+from .exceptions import NotFound
 
 
-class Todo(db.Model):
+class Model(db.Model):
+    __abstract__ = True
+
+    # def __init__(self, **kwargs):
+    #     super(Model, self).__init__(**kwargs)
+
+    @classmethod
+    def get(cls, id):
+        model = cls.query.get(id)
+        if model:
+            return model
+        raise NotFound
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+    def create(self):
+        db.session.add(self)
+        self.save()
+        return self
+
+    def save(self):
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    def to_dict(self, include=[], exclude=[]):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns if c.name not in exclude or c.name in include}
+
+
+class Task(Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
 
+    def __init__(self, content) -> None:
+        super().__init__()
+        self.content = content
+        self.date_created = datetime.utcnow()
+
     def __repr__(self):
         return '<Task {self.id} was successfully created>'
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'content': self.content,
-            'completed': self.completed,
-            'date_created': self.date_created.date()
-        }
