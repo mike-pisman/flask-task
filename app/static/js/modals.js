e31cf579949f1792a8e8cfc5cb594147b21b17f3
Michael Pisman <mike.pisman@gmail.com>
2023-10-30 23:21:16 -0600

    refactor: Changed the way task is is stored on the html page

--- app/static/js/modals.js
+++ app/static/js/modals.js
@@ -1,8 +1,9 @@
 $('#editTask').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
-    var id = button.data('taskid')
+    var id = button.parent().parent().parent().data('task-id')
+
     var modal = $(this)
-    var content = $("#task"+id).html()
+    var content = $("#task_"+id).find('.task-content').html()
     modal.find('#editTaskForm textarea').html(content)
     modal.find('#editTaskForm').attr('action', 'update/' + id);
 })
