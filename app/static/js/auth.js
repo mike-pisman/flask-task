737659893adce5f90f0d09ea2741e7b8deabe33e
Michael Pisman <mike.pisman@gmail.com>
2023-11-02 22:51:26 -0600

    Add login request and form validation

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -1,6 +1,10 @@
-signup = function(){ 
+signup = function(){
     form = $('#form-signup');
-    formData = form.serialize();
+    formData = {
+        email: $('#email').val(),
+        name: $('#name').val(),
+        password: $('#password').val()
+    }
 
     $.ajax({
         url: '/signup',
@@ -12,8 +16,46 @@ signup = function(){
         },
         error: function (data) {
             response = JSON.parse(data.responseText);
-            console.log(response);
             addAlert(response.error, "danger");
         }
     });
 }
+
+login = function(){ 
+    form = $('#form-login');
+    formData = form.serialize();
+
+    formData = {
+        email: $('#email').val(),
+        password: $('#password').val(),
+        remember: $('#remember-me').is(':checked')
+    }
+
+    console.log(formData);
+
+    $.ajax({
+        url: '/login',
+        type: 'POST',
+        accepts: 'application/json',
+        data: formData,
+        success: function (data) {
+            window.open("/", "_self");
+        },
+        error: function (data) {
+            response = JSON.parse(data.responseText);
+            addAlert(response.error, "danger");
+        }
+    });
+}
+
+$(document).ready(function() {
+    $('#form-login').submit(function(e){
+        e.preventDefault();
+        login();
+    });
+
+    $('#form-signup').submit(function(e){
+        e.preventDefault();
+        signup();
+    });
+});
