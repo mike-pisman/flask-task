80b63977945307527104b1c614d683b5ba88159a
Michael Pisman <mike.pisman@gmail.com>
2023-11-12 23:36:41 -0700

    Refactor task routes to use task list ID

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -4,70 +4,75 @@ from app.models.task_list import TaskList
 from app.exceptions import NotFound
 from app.routes import JSONResponse, login_required_json
 
-routes = Blueprint('task_routes', __name__)
+routes = Blueprint("task_routes", __name__)
 
 
-@routes.route("/tasks", methods=['GET'])
+@routes.route("/api/lists/<int:list_id>/tasks", methods=["GET"])
 @login_required_json
-def get_tasks():
+def get_tasks(list_id):
     try:
-        tasks = Task.get_all()
-        return JSONResponse({'tasks': [task.to_dict() for task in tasks]}, 200)
-    except Exception:
-        return JSONResponse({'error': 'Internal Server Error'}, 500)
-
-
-@routes.route('/tasks', methods=['POST'])
-@login_required_json
-def create_task():
-    try:
-        task_content = request.form['content']
-        task_list_id = request.form['list_id']
-        task_list = TaskList.get(task_list_id)
-        new_task = Task(content=task_content).create()
-        task_list.add_task(new_task)
-        return JSONResponse({'task': new_task.to_dict()}, 201)
+        # tasks = Task.get_all().filter_by(task_list_id=list_id)
+        tasks = TaskList.get(list_id).get_tasks()
+        return JSONResponse({"tasks": [task.to_dict() for task in tasks]}, 200)
     except NotFound:
-        return JSONResponse({'error': 'Task list not found'}, 404)
-    except Exception:
-        return JSONResponse({'error': 'Internal Server Error'}, 500)
+        return JSONResponse({"error": "Task list not found"}, 404)
+    except Exception as e:
+        print(e)
+        return JSONResponse({"error": "Internal Server Error"}, 500)
 
 
-@routes.route('/tasks/<int:id>', methods=['POST'])
+@routes.route("/api/lists/<int:list_id>/tasks", methods=["POST"])
 @login_required_json
-def complete_task(id):
+def create_task(list_id):
+    try:
+        task_content = request.form["content"]
+        task_list = TaskList.get(list_id)
+        new_task = Task(content=task_content,
+                        task_list_id=task_list.id).create()
+        # task_list.add_task(new_task)
+        return JSONResponse({"task": new_task.to_dict()}, 201)
+    except NotFound:
+        return JSONResponse({"error": "Task list not found"}, 404)
+    except Exception as e:
+        print(e)
+        return JSONResponse({"error": "Internal Server Error"}, 500)
+
+
+@routes.route("/api/lists/<int:list_id>/tasks/<int:id>", methods=["POST"])
+@login_required_json
+def complete_task(list_id, id):
     try:
         task = Task.get(id)
         task.completed = not task.completed
         task.save()
-        return JSONResponse({'task': task.to_dict()}, 200)
+        return JSONResponse({"task": task.to_dict()}, 200)
     except NotFound:
-        return JSONResponse({'error': 'Task not found'}, 404)
+        return JSONResponse({"error": "Task not found"}, 404)
     except Exception:
-        return JSONResponse({'error': 'Internal Server Error'}, 500)
+        return JSONResponse({"error": "Internal Server Error"}, 500)
 
 
-@routes.route('/tasks/<int:id>', methods=['PATCH'])
+@routes.route("/api/lists/<int:list_id>/tasks/<int:id>", methods=["PATCH"])
 @login_required_json
-def update_task(id):
+def update_task(list_id, id):
     try:
         task = Task.get(id)
-        task.content = request.form['content']
+        task.content = request.form["content"]
         task.save()
-        return JSONResponse({'task': task.to_dict()}, 200)
+        return JSONResponse({"task": task.to_dict()}, 200)
     except NotFound:
-        return JSONResponse({'error': 'Task not found'}, 404)
+        return JSONResponse({"error": "Task not found"}, 404)
     except Exception:
-        return JSONResponse({'error': 'Internal Server Error'}, 500)
+        return JSONResponse({"error": "Internal Server Error"}, 500)
 
 
-@routes.route('/tasks/<int:id>', methods=['DELETE'])
+@routes.route("/api/lists/<int:list_id>/tasks/<int:id>", methods=["DELETE"])
 @login_required_json
-def delete_task(id):
+def delete_task(list_id, id):
     try:
         Task.get(id).delete()
         return JSONResponse({}, 204)
     except NotFound:
-        return JSONResponse({'error': 'Task not found'}, 404)
+        return JSONResponse({"error": "Task not found"}, 404)
     except Exception:
-        return JSONResponse({'error': 'Internal Server Error'}, 500)
+        return JSONResponse({"error": "Internal Server Error"}, 500)
