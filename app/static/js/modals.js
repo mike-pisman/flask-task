1de42b2b4b11484c4d95984d70c49e5a84a79a79
Michael Pisman <mike.pisman@gmail.com>
2023-11-12 23:39:44 -0700

    Update modals.js with new event listeners for edit and delete functionality

--- app/static/js/modals.js
+++ app/static/js/modals.js
@@ -1,10 +1,29 @@
-$('#editTask').on('show.bs.modal', function (event) {
+$('#editTaskModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var id = button.parent().parent().parent().data('task-id')
-
-    var modal = $(this)
     var content = $("#task_"+id).find('.task-content').html()
     $('#editTaskForm').find('textarea').html(content)
     $('#editTaskForm').attr('action', 'tasks/' + id);
     $('#editTaskForm').attr('data-task-id', id);
+})
+
+$('#editListModal').on('show.bs.modal', function (event) {
+    let button = $(event.relatedTarget) // Button that triggered the modal
+    let id = button.parent().parent().parent().data('list-id')
+    let name = $("#list_"+id).find('.list-name').html()
+    $('#editListForm').find('input').val(name)
+    $('#editListForm').attr('action', 'lists/' + id);
+    $('#editListForm').attr('data-list-id', id);
+})
+
+$('#deleteTaskModal').on('show.bs.modal', function (event) {
+    let button = $(event.relatedTarget) // Button that triggered the modal
+    let id = button.parent().parent().parent().data('task-id')
+    $('#deleteTaskModal').data('task-id', id);
+})
+
+$('#deleteListModal').on('show.bs.modal', function (event) {
+    let button = $(event.relatedTarget) // Button that triggered the modal
+    let id = button.parent().parent().parent().data('list-id')
+    $('#deleteListModal').data('list-id', id);
 })
