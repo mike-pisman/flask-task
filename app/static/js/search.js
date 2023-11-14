dc502dacd408898f9f0fa1ff18ce5e1814b7d5cb
Michael Pisman <mike.pisman@gmail.com>
2023-11-13 23:52:16 -0700

    Update search functionality for tasks

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -20,3 +20,26 @@ $(function(){
         });
     });
 });
+
+$(function(){
+    $("#taskSearchInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase().split(" ");
+
+        $("table tr").each(function(index) {
+            if (index != 0) {
+
+                $row = $(this);
+
+                var name = $row.find(".task-content").text();
+
+                $(this).show();
+                for (i in value) {
+                    if (name.toLowerCase().indexOf(value[i]) == -1) {
+                        $(this).hide();
+                        break;
+                    }
+                }
+            }
+        });
+    });
+});
